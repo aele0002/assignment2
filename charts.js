1995be3806bc37c78d04a6d9c8a8c7825903db55
aele0002 <92426762+aele0002@users.noreply.github.com>
2021-10-15 12:28:19 +0700

    Add files via upload

---
+++ charts.js
@@ -0,0 +1,10 @@
+var vg_1 = "./piechart.vg.json";
+var vg_2 = "./bubbleChart.vg.json";
+
+vegaEmbed("#PieChart",vg_1).then(function(result){
+
+}).catch(console.error);
+
+vegaEmbed("#bubbleChart",vg_2).then(function(result){
+
+}).catch(console.error);
